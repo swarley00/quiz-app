18c4849e77d573b955dd14fc2ba3513df055ddc5
swarley00 <jason.caryl@gmail.com>
2015-05-26 12:24:41 -0400

    Added the rest of the quizItem objects and commented them out

--- js/script.js
+++ js/script.js
@@ -6,6 +6,10 @@ $(document).ready(function() {
 	}
 
 	var question1 = new quizItem ('How does Barry Allen gain his powers?', 'Struck By Lightning', ['Kryptonite', 'An Alien Ring', 'Lab Experiments']);
+	// var question2 = new quizItem ('?', '', ['', '', '']);
+	// var question3 = new quizItem ('?', '', ['', '', '']);
+	// var question4 = new quizItem ('?', '', ['', '', '']);
+	// var question5 = new quizItem ('?', '', ['', '', '']);
 	// var randomArray = [Math.floor(Math.random()*question1.wrongAnswers.length)];
 	
 	$('.question').text(question1.question);
@@ -13,4 +17,37 @@ $(document).ready(function() {
 	$('.wronganswer1').text(question1.wrongAnswers[0]);
 	$('.wronganswer2').text(question1.wrongAnswers[1]);
 	$('.wronganswer3').text(question1.wrongAnswers[2]);
+
+	// $('.question').text(question2.question);
+	// $('.answer').text(question2.answer);
+	// $('.wronganswer1').text(question2.wrongAnswers[0]);
+	// $('.wronganswer2').text(question2.wrongAnswers[1]);
+	// $('.wronganswer3').text(question2.wrongAnswers[2]);
+
+	// $('.question').text(question3.question);
+	// $('.answer').text(question3.answer);
+	// $('.wronganswer1').text(question3.wrongAnswers[0]);
+	// $('.wronganswer2').text(question3.wrongAnswers[1]);
+	// $('.wronganswer3').text(question3.wrongAnswers[2]);
+
+	// $('.question').text(question4.question);
+	// $('.answer').text(question4.answer);
+	// $('.wronganswer1').text(question4.wrongAnswers[0]);
+	// $('.wronganswer2').text(question4.wrongAnswers[1]);
+	// $('.wronganswer3').text(question4.wrongAnswers[2]);
+
+	// $('.question').text(question5.question);
+	// $('.answer').text(question5.answer);
+	// $('.wronganswer1').text(question5.wrongAnswers[0]);
+	// $('.wronganswer2').text(question5.wrongAnswers[1]);
+	// $('.wronganswer3').text(question5.wrongAnswers[2]);
+
+	// $(#submitBtn).click(function() {
+	// 	if () {
+
+	// 	}
+	// 	else {
+			
+	// 	} 
+	// })
 });
