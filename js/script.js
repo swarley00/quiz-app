5ba013d98c5da74ba6519deebe32ef3b6efbe0d5
swarley00 <jason.caryl@gmail.com>
2015-05-26 17:02:33 -0400

    Add toggleClass functionality

--- js/script.js
+++ js/script.js
@@ -1,15 +1,15 @@
 $(document).ready(function() {
-	function quizItem (question, answer, wrongAnswers) {
+	function QuizItem (question, answer, wrongAnswers) {
 		this.question = question;
 		this.answer = answer;
 		this.wrongAnswers = wrongAnswers;
 	}
 
-	var question1 = new quizItem ('How does Barry Allen gain his powers?', 'Struck By Lightning', ['Kryptonite', 'An Alien Ring', 'Lab Experiments']);
-	// var question2 = new quizItem ('?', '', ['', '', '']);
-	// var question3 = new quizItem ('?', '', ['', '', '']);
-	// var question4 = new quizItem ('?', '', ['', '', '']);
-	// var question5 = new quizItem ('?', '', ['', '', '']);
+	var question1 = new QuizItem ('How does Barry Allen gain his powers?', 'Struck By Lightning', ['Kryptonite', 'An Alien Ring', 'Lab Experiments']);
+	var question2 = new QuizItem ('What comic did Barry first appear in?', 'Showcase #4', ['Flash Comics #1', 'Superman #199', 'Flash #167']);
+	var question3 = new QuizItem ("Who is Barry's archenemy?", 'Reverse-Flash', ['Captain Cold', 'The Joker', 'Sinestro']);
+	var question4 = new QuizItem ('In what storyline does Barry die?', 'Crisis on Infinite Earths', ['Final Crisis', 'Flash: Rebirth', 'Flashpoint']);
+	var question5 = new QuizItem ('In what publication year did Barry return to the DCU?', '2008', ['1976', '2004', '2012']);
 	// var randomArray = [Math.floor(Math.random()*question1.wrongAnswers.length)];
 	
 	$('.question').text(question1.question);
@@ -17,6 +17,20 @@ $(document).ready(function() {
 	$('.wronganswer1').text(question1.wrongAnswers[0]);
 	$('.wronganswer2').text(question1.wrongAnswers[1]);
 	$('.wronganswer3').text(question1.wrongAnswers[2]);
+
+	$('li').click(function() {
+		$(this).closest('li').find('p').toggleClass('selected');
+		console.log(this);
+	});
+
+	// $(#submitBtn).click(function() {
+	// 	if (.) {
+
+	// 	}
+	// 	else {
+
+	// 	} 
+	// })
 
 	// $('.question').text(question2.question);
 	// $('.answer').text(question2.answer);
@@ -41,13 +55,4 @@ $(document).ready(function() {
 	// $('.wronganswer1').text(question5.wrongAnswers[0]);
 	// $('.wronganswer2').text(question5.wrongAnswers[1]);
 	// $('.wronganswer3').text(question5.wrongAnswers[2]);
-
-	// $(#submitBtn).click(function() {
-	// 	if () {
-
-	// 	}
-	// 	else {
-			
-	// 	} 
-	// })
 });
