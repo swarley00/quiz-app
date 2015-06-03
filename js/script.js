a36fe35610b8ed4b4af8c2ba0c0fd343c1c8a497
swarley00 <jason.caryl@gmail.com>
2015-06-03 17:13:34 -0400

    Set up question functions

--- js/script.js
+++ js/script.js
@@ -25,6 +25,7 @@ $(document).ready(function() {
 				console.log("right answer");
 
 				$('.overlay-container').show();	
+				$('h2').text("You're Correct!");
 				$('.pop-out img').attr('src', 'images/pop-up.jpg');
 				$('.overlay-container p').text('After graduating, Allen found work with the Central City Police Department as a police scientist, with a well-known habit for slowness and lateness. Working late in the lab one night, a rack of chemicals that he was working next to was struck by lightning, and Allen was doused in chemicals. Surprisingly unharmed, Allen cleaned up the mess and returned to work, but soon noticed that the world appeared to be moving much slower than normal. He soon realized that it was not the world that had slowed, but himself who had sped up.');
 				
@@ -35,6 +36,7 @@ $(document).ready(function() {
 						'background-color': '#f2bc1a',
 						'border': '2px solid #f2d264',
 						'border-radius': '2em'});
+					$("input:checked").prop('checked', false);
 				});
 			}
 			else {
@@ -42,6 +44,9 @@ $(document).ready(function() {
 
 				$('.overlay-container').show();
 				$('h2').text('You Were So Close...');
+				$('.pop-out img').attr('src', 'images/pop-up.jpg');
+				$('.overlay-container p').text('After graduating, Allen found work with the Central City Police Department as a police scientist, with a well-known habit for slowness and lateness. Working late in the lab one night, a rack of chemicals that he was working next to was struck by lightning, and Allen was doused in chemicals. Surprisingly unharmed, Allen cleaned up the mess and returned to work, but soon noticed that the world appeared to be moving much slower than normal. He soon realized that it was not the world that had slowed, but himself who had sped up.');
+				
 				$('#nextBtn').click(function() {
 					$('.overlay-container').hide();
 					$('.question-number').find('li:nth-child(2)').css({
@@ -49,10 +54,9 @@ $(document).ready(function() {
 						'background-color': '#f2bc1a',
 						'border': '2px solid #f2d264',
 						'border-radius': '2em'});
+					$("input:checked").prop('checked', false);
 				});
 			}
-
-			$("input:checked").prop('checked', false);
 			Question2();
 		});
 	}
@@ -67,9 +71,14 @@ $(document).ready(function() {
 		$('.wronganswer3').text(question2.wrongAnswers[2]);
 
 		$('#submitBtn').click(function() {
-			if ($('.selected').is('.answer')) {
-				$('.overlay-container').show();
+			if($('input:checked').val() === "B") {
+				console.log("right answer");
+
+				$('.overlay-container').show();	
+				$('h2').text("You're Correct!");
+				$('.pop-out img').attr('src', 'images/showcase4.jpg');
 				$('.overlay-container p').text('Barry Allen was created by Robert Kanigher, John Broome and Carmine Infantino. He made his first comic book appearance in October 1956 in the pages of Showcase #4.');
+				
 				$('#nextBtn').click(function() {
 					$('.overlay-container').hide();
 					$('.question-number').find('li:nth-child(3)').css({
@@ -77,12 +86,17 @@ $(document).ready(function() {
 						'background-color': '#f2bc1a',
 						'border': '2px solid #f2d264',
 						'border-radius': '2em'});
-					Question3();
+					$("input:checked").prop('checked', false);
 				});
 			}
 			else {
+				console.log("wrong answer");
+
 				$('.overlay-container').show();
 				$('h2').text('You Were So Close...');
+				$('.pop-out img').attr('src', 'images/showcase4.jpg');
+				$('.overlay-container p').text('Barry Allen was created by Robert Kanigher, John Broome and Carmine Infantino. He made his first comic book appearance in October 1956 in the pages of Showcase #4.');
+			
 				$('#nextBtn').click(function() {
 					$('.overlay-container').hide();
 					$('.question-number').find('li:nth-child(3)').css({
@@ -90,131 +104,161 @@ $(document).ready(function() {
 						'background-color': '#f2bc1a',
 						'border': '2px solid #f2d264',
 						'border-radius': '2em'});
-					Question3();
+					$("input:checked").prop('checked', false);
 				});
-			} 
+			}
+			Question3();
 		});
 	}
 
-// 	function Question3 () {
-// 		var question3 = new QuizItem ("Who is Barry's archenemy?", 'Reverse-Flash', ['Captain Cold', 'Joker', 'Sinestro']);
+	function Question3 () {
+		var question3 = new QuizItem ("Who is Barry's archenemy?", 'Reverse-Flash', ['Captain Cold', 'Joker', 'Sinestro']);
 
-// 		$('.question').text(question3.question);
-// 		$('.answer').text(question3.answer);
-// 		$('.wronganswer1').text(question3.wrongAnswers[0]);
-// 		$('.wronganswer2').text(question3.wrongAnswers[1]);
-// 		$('.wronganswer3').text(question3.wrongAnswers[2]);
+		$('.question').text(question3.question);
+		$('.answer').text(question3.answer);
+		$('.wronganswer1').text(question3.wrongAnswers[0]);
+		$('.wronganswer2').text(question3.wrongAnswers[1]);
+		$('.wronganswer3').text(question3.wrongAnswers[2]);
 
-// 		$('#submitBtn').click(function() {
-// 			if ($('.selected').is('.answer')) {
-// 				$('.overlay-container').show();
-// 				$('#nextBtn').click(function() {
-// 					$('.overlay-container').hide();
-// 					$('.question-number').find('li:nth-child(4)').css({
-// 						'color': 'white',
-// 						'background-color': '#f2bc1a',
-// 						'border': '2px solid #f2d264',
-// 						'border-radius': '2em'});
-// 					Question4();
-// 				});
-// 			}
-// 			else {
-// 				$('.overlay-container').show();
-// 				$('h2').text('You Were So Close...');
-// 				$('#nextBtn').click(function() {
-// 					$('.overlay-container').hide();
-// 					$('.question-number').find('li:nth-child(4)').css({
-// 						'color': 'white',
-// 						'background-color': '#f2bc1a',
-// 						'border': '2px solid #f2d264',
-// 						'border-radius': '2em'});
-// 					Question4();
-// 				});
-// 			} 
-// 		});
-// 	}
+		$('#submitBtn').click(function() {
+			if($('input:checked').val() === "B") {
+				console.log("right answer");
 
-// 	function Question4 () {
-// 		var question4 = new QuizItem ('In what storyline does Barry die?', 'Crisis on Infinite Earths', ['Final Crisis', 'Flash: Rebirth', 'Flashpoint']);
+				$('.overlay-container').show();	
+				$('h2').text("You're Correct!");
+				$('.pop-out img').attr('src', 'images/'); //need image
+				$('.overlay-container p').text(''); //need text
+				
+				$('#nextBtn').click(function() {
+					$('.overlay-container').hide();
+					$('.question-number').find('li:nth-child(4)').css({
+						'color': 'white',
+						'background-color': '#f2bc1a',
+						'border': '2px solid #f2d264',
+						'border-radius': '2em'});
+					$("input:checked").prop('checked', false);
+				});
+			}
+			else {
+				console.log("wrong answer");
 
-// 		$('.question').text(question4.question);
-// 		$('.answer').text(question4.answer);
-// 		$('.wronganswer1').text(question4.wrongAnswers[0]);
-// 		$('.wronganswer2').text(question4.wrongAnswers[1]);
-// 		$('.wronganswer3').text(question4.wrongAnswers[2]);	
+				$('.overlay-container').show();
+				$('h2').text('You Were So Close...');
+				$('.pop-out img').attr('src', 'images/'); //need image
+				$('.overlay-container p').text(''); //need text
+			
+				$('#nextBtn').click(function() {
+					$('.overlay-container').hide();
+					$('.question-number').find('li:nth-child(4)').css({
+						'color': 'white',
+						'background-color': '#f2bc1a',
+						'border': '2px solid #f2d264',
+						'border-radius': '2em'});
+					$("input:checked").prop('checked', false);
+				});
+			}
+			Question4();
+		});
+	}
 
-// 		$('#submitBtn').click(function() {
-// 			if ($('.selected').is('.answer')) {
-// 				$('.overlay-container').show();
-// 				$('#nextBtn').click(function() {
-// 					$('.overlay-container').hide();
-// 					$('.question-number').find('li:nth-child(5)').css({
-// 						'color': 'white',
-// 						'background-color': '#f2bc1a',
-// 						'border': '2px solid #f2d264',
-// 						'border-radius': '2em'});
-// 					Question5();
-// 				});
-// 			}
-// 			else {
-// 				$('.overlay-container').show();
-// 				$('h2').text('You Were So Close...');
-// 				$('#nextBtn').click(function() {
-// 					$('.overlay-container').hide();
-// 					$('.question-number').find('li:nth-child(5)').css({
-// 						'color': 'white',
-// 						'background-color': '#f2bc1a',
-// 						'border': '2px solid #f2d264',
-// 						'border-radius': '2em'});
-// 					Question5();
-// 				});
-// 			} 
-// 		});
-// 	}
+	function Question4 () {
+		var question4 = new QuizItem ('In what storyline does Barry die?', 'Crisis on Infinite Earths', ['Final Crisis', 'Flash: Rebirth', 'Flashpoint']);
 
-// 	function Question5 () {
-// 		var question5 = new QuizItem ('In what publication year did Barry return to the DCU?', '2008', ['1976', '2004', '2012']);
+		$('.question').text(question4.question);
+		$('.answer').text(question4.answer);
+		$('.wronganswer1').text(question4.wrongAnswers[0]);
+		$('.wronganswer2').text(question4.wrongAnswers[1]);
+		$('.wronganswer3').text(question4.wrongAnswers[2]);	
 
-// 		$('.question').text(question5.question);
-// 		$('.answer').text(question5.answer);
-// 		$('.wronganswer1').text(question5.wrongAnswers[0]);
-// 		$('.wronganswer2').text(question5.wrongAnswers[1]);
-// 		$('.wronganswer3').text(question5.wrongAnswers[2]);
+		$('#submitBtn').click(function() {
+			if($('input:checked').val() === "B") {
+				console.log("right answer");
 
-// 		$('#submitBtn').click(function() {
-// 			if ($('.selected').is('.answer')) {
-// 				$('.overlay-container').show();
-// 				$('#nextBtn').click(function() {
-// 					$('.overlay-container').hide();
-// 					$('.question-number').find('li:nth-child(5)').css({
-// 						'color': 'white',
-// 						'background-color': '#f2bc1a',
-// 						'border': '2px solid #f2d264',
-// 						'border-radius': '2em'});
-// 				});
-// 			}
-// 			else {
-// 				$('.overlay-container').show();
-// 				$('h2').text('You Were So Close...');
-// 				$('#nextBtn').click(function() {
-// 					$('.overlay-container').hide();
-// 					$('.question-number').find('li:nth-child(5)').css({
-// 						'color': 'white',
-// 						'background-color': '#f2bc1a',
-// 						'border': '2px solid #f2d264',
-// 						'border-radius': '2em'});
-// 				});
-// 			} 
-// 		});
-// 	}
-});
+				$('.overlay-container').show();	
+				$('h2').text("You're Correct!");
+				$('.pop-out img').attr('src', 'images/'); //need image
+				$('.overlay-container p').text(''); //need text
+				
+				$('#nextBtn').click(function() {
+					$('.overlay-container').hide();
+					$('.question-number').find('li:nth-child(5)').css({
+						'color': 'white',
+						'background-color': '#f2bc1a',
+						'border': '2px solid #f2d264',
+						'border-radius': '2em'});
+					$("input:checked").prop('checked', false);
+				});
+			}
+			else {
+				console.log("wrong answer");
 
-	// var randomArray = [Math.floor(Math.random()*question1.wrongAnswers.length)];
+				$('.overlay-container').show();
+				$('h2').text('You Were So Close...');
+				$('.pop-out img').attr('src', 'images/'); //need image
+				$('.overlay-container p').text(''); //need text
+			
+				$('#nextBtn').click(function() {
+					$('.overlay-container').hide();
+					$('.question-number').find('li:nth-child(5)').css({
+						'color': 'white',
+						'background-color': '#f2bc1a',
+						'border': '2px solid #f2d264',
+						'border-radius': '2em'});
+					$("input:checked").prop('checked', false);
+				});
+			}
+			Question5();
+		});
+	}
 
-	// function PopUp (image, headline, bodycopy) {
-	// 	this.image = image;
-	// 	this.headline = headline;
-	// 	this.bodycopy = bodycopy;
-	// } 
+	function Question5 () {
+		var question5 = new QuizItem ('In what publication year did Barry return to the DCU?', '2008', ['1976', '2004', '2012']);
 
-	// var popup1 = new PopUp ();
+		$('.question').text(question5.question);
+		$('.answer').text(question5.answer);
+		$('.wronganswer1').text(question5.wrongAnswers[0]);
+		$('.wronganswer2').text(question5.wrongAnswers[1]);
+		$('.wronganswer3').text(question5.wrongAnswers[2]);
+
+		$('#submitBtn').click(function() {
+			if($('input:checked').val() === "B") {
+				console.log("right answer");
+
+				$('.overlay-container').show();	
+				$('h2').text("You're Correct!");
+				$('.pop-out img').attr('src', 'images/'); //need image
+				$('.overlay-container p').text(''); //need text
+				
+				$('#nextBtn').click(function() {
+					$('.overlay-container').hide();
+					$('.question-number').find('li:nth-child(6)').css({
+						'color': 'white',
+						'background-color': '#f2bc1a',
+						'border': '2px solid #f2d264',
+						'border-radius': '2em'});
+					$("input:checked").prop('checked', false);
+				});
+			}
+			else {
+				console.log("wrong answer");
+
+				$('.overlay-container').show();
+				$('h2').text('You Were So Close...');
+				$('.pop-out img').attr('src', 'images/'); //need image
+				$('.overlay-container p').text(''); //need text
+			
+				$('#nextBtn').click(function() {
+					$('.overlay-container').hide();
+					$('.question-number').find('li:nth-child(6)').css({
+						'color': 'white',
+						'background-color': '#f2bc1a',
+						'border': '2px solid #f2d264',
+						'border-radius': '2em'});
+					$("input:checked").prop('checked', false);
+				});
+			}
+		});
+	}
+}); //ending bracket
+
+// var randomArray = [Math.floor(Math.random()*question1.wrongAnswers.length)];
