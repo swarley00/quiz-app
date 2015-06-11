5debea848ad1884d0a08e7c26ffddd9f8a2d18fb
Jason Caryl <jason.caryl@gmail.com>
2015-06-10 21:50:39 -0400

    Add randomize logic for each question and fix css on the question number

--- js/script.js
+++ js/script.js
@@ -31,12 +31,9 @@ $(document).ready(function() {
 		$('.question').text(question1.question);
 
 		var answers = [question1.answer, question1.wrongAnswers[0], question1.wrongAnswers[1], question1.wrongAnswers[2]];
-		// answers[0] = '<p class="wronganswer1">' + question1.wrongAnswers[0] + '</p>';
-		// answers[1] = '<p class="answer">' + question1.answer + '</p>';
-		// answers[2] = '<p class="wronganswer2">' + question1.wrongAnswers[1] + '</p>';
-		// answers[3] = '<p class="wronganswer3">' + question1.wrongAnswers[2] + '</p>';
 
-		console.log(answers);
+		// console.log(answers);
+		console.log(question1.answer);
 		shuffle(answers);
 
 		$('#1 .answer-text').text(answers[0]);
@@ -51,12 +48,6 @@ $(document).ready(function() {
 		$('#4 .answer-text').text(answers[3]);
 		$('#4 input').val(answers[3]);
 
-		// shuffle(answers);
-		// $('#1').append(answers[0]);
-		// $('#2').append(answers[1]);
-		// $('#3').append(answers[2]);
-		// $('#4').append(answers[3]);
-
 		$('#submitBtn').click(function() {
 			if($('input:checked').val() === question1.answer) {
 				console.log("right answer");
@@ -68,11 +59,6 @@ $(document).ready(function() {
 				
 				$('#nextBtn').click(function() {
 					$('.overlay-container').hide();
-					//$('.question-number').find('li:nth-child(2)').css({
-					// 	'color': 'white',
-					// 	'background-color': '#f2bc1a',
-					// 	'border': '2px solid #f2d264',
-					// 	'border-radius': '2em'});
 					$("input:checked").prop('checked', false);
 				});
 			}
@@ -86,11 +72,6 @@ $(document).ready(function() {
 				
 				$('#nextBtn').click(function() {
 					$('.overlay-container').hide();
-					// $('.question-number').find('li:nth-child(2)').css({
-					// 	'color': 'white',
-					// 	'background-color': '#f2bc1a',
-					// 	'border': '2px solid #f2d264',
-					// 	'border-radius': '2em'});
 					$("input:checked").prop('checked', false);
 				});
 			}
@@ -101,15 +82,28 @@ $(document).ready(function() {
 
 	function Question2 () {
 		var question2 = new QuizItem ('What comic did Barry first appear in?', 'Showcase #4', ['Flash Comics #1', 'Superman #199', 'Flash #167']);
-
 		$('.question').text(question2.question);
-		$('.answer').text(question2.answer);
-		$('.wronganswer1').text(question2.wrongAnswers[0]);
-		$('.wronganswer2').text(question2.wrongAnswers[1]);
-		$('.wronganswer3').text(question2.wrongAnswers[2]);
+
+		var answers = [question2.answer, question2.wrongAnswers[0], question2.wrongAnswers[1], question2.wrongAnswers[2]];
+
+		// console.log(answers);
+		console.log(question2.answer);
+		shuffle(answers);
+
+		$('#1 .answer-text').text(answers[0]);
+		$('#1 input').val(answers[0]);
+
+		$('#2 .answer-text').text(answers[1]);
+		$('#2 input').val(answers[1]);
+
+		$('#3 .answer-text').text(answers[2]);
+		$('#3 input').val(answers[2]);
+
+		$('#4 .answer-text').text(answers[3]);
+		$('#4 input').val(answers[3]);
 
 		$('#submitBtn').click(function() {
-			if($('input:checked').val() === "B") {
+			if($('input:checked').val() === question2.answer) {
 				console.log("right answer");
 
 				$('.overlay-container').show();	
@@ -119,11 +113,6 @@ $(document).ready(function() {
 				
 				$('#nextBtn').click(function() {
 					$('.overlay-container').hide();
-					$('.question-number').find('li:nth-child(3)').css({
-						'color': 'white',
-						'background-color': '#f2bc1a',
-						'border': '2px solid #f2d264',
-						'border-radius': '2em'});
 					$("input:checked").prop('checked', false);
 				});
 			}
@@ -137,29 +126,38 @@ $(document).ready(function() {
 			
 				$('#nextBtn').click(function() {
 					$('.overlay-container').hide();
-					$('.question-number').find('li:nth-child(3)').css({
-						'color': 'white',
-						'background-color': '#f2bc1a',
-						'border': '2px solid #f2d264',
-						'border-radius': '2em'});
 					$("input:checked").prop('checked', false);
 				});
 			}
+			$('.question-number').find('li:nth-child(3)').addClass('active-number');
 			Question3();
 		});
 	}
 
 	function Question3 () {
 		var question3 = new QuizItem ("Who is Barry's archenemy?", 'Professor Zoom', ['Captain Cold', 'Joker', 'Sinestro']);
-
 		$('.question').text(question3.question);
-		$('.answer').text(question3.answer);
-		$('.wronganswer1').text(question3.wrongAnswers[0]);
-		$('.wronganswer2').text(question3.wrongAnswers[1]);
-		$('.wronganswer3').text(question3.wrongAnswers[2]);
+
+		var answers = [question3.answer, question3.wrongAnswers[0], question3.wrongAnswers[1], question3.wrongAnswers[2]];
+
+		// console.log(answers);
+		console.log(question3.answer);
+		shuffle(answers);
+
+		$('#1 .answer-text').text(answers[0]);
+		$('#1 input').val(answers[0]);
+
+		$('#2 .answer-text').text(answers[1]);
+		$('#2 input').val(answers[1]);
+
+		$('#3 .answer-text').text(answers[2]);
+		$('#3 input').val(answers[2]);
+
+		$('#4 .answer-text').text(answers[3]);
+		$('#4 input').val(answers[3]);
 
 		$('#submitBtn').click(function() {
-			if($('input:checked').val() === "B") {
+			if($('input:checked').val() === question3.answer) {
 				console.log("right answer");
 
 				$('.overlay-container').show();	
@@ -169,11 +167,6 @@ $(document).ready(function() {
 
 				$('#nextBtn').click(function() {
 					$('.overlay-container').hide();
-					$('.question-number').find('li:nth-child(4)').css({
-						'color': 'white',
-						'background-color': '#f2bc1a',
-						'border': '2px solid #f2d264',
-						'border-radius': '2em'});
 					$("input:checked").prop('checked', false);
 				});
 			}
@@ -187,29 +180,38 @@ $(document).ready(function() {
 
 				$('#nextBtn').click(function() {
 					$('.overlay-container').hide();
-					$('.question-number').find('li:nth-child(4)').css({
-						'color': 'white',
-						'background-color': '#f2bc1a',
-						'border': '2px solid #f2d264',
-						'border-radius': '2em'});
 					$("input:checked").prop('checked', false);
 				});
 			}
+			$('.question-number').find('li:nth-child(4)').addClass('active-number');
 			Question4();
 		});
 	}
 
 	function Question4 () {
 		var question4 = new QuizItem ('In what storyline does Barry die?', 'Crisis on Infinite Earths', ['Final Crisis', 'Flash: Rebirth', 'Flashpoint']);
-
 		$('.question').text(question4.question);
-		$('.answer').text(question4.answer);
-		$('.wronganswer1').text(question4.wrongAnswers[0]);
-		$('.wronganswer2').text(question4.wrongAnswers[1]);
-		$('.wronganswer3').text(question4.wrongAnswers[2]);	
+		
+		var answers = [question4.answer, question4.wrongAnswers[0], question4.wrongAnswers[1], question4.wrongAnswers[2]];
+
+		// console.log(answers);
+		console.log(question4.answer);
+		shuffle(answers);
+
+		$('#1 .answer-text').text(answers[0]);
+		$('#1 input').val(answers[0]);
+
+		$('#2 .answer-text').text(answers[1]);
+		$('#2 input').val(answers[1]);
+
+		$('#3 .answer-text').text(answers[2]);
+		$('#3 input').val(answers[2]);
+
+		$('#4 .answer-text').text(answers[3]);
+		$('#4 input').val(answers[3]);
 
 		$('#submitBtn').click(function() {
-			if($('input:checked').val() === "B") {
+			if($('input:checked').val() === question4.answer) {
 				console.log("right answer");
 
 				$('.overlay-container').show();	
@@ -219,11 +221,6 @@ $(document).ready(function() {
 				
 				$('#nextBtn').click(function() {
 					$('.overlay-container').hide();
-					$('.question-number').find('li:nth-child(5)').css({
-						'color': 'white',
-						'background-color': '#f2bc1a',
-						'border': '2px solid #f2d264',
-						'border-radius': '2em'});
 					$("input:checked").prop('checked', false);
 				});
 			}
@@ -237,43 +234,51 @@ $(document).ready(function() {
 			
 				$('#nextBtn').click(function() {
 					$('.overlay-container').hide();
-					$('.question-number').find('li:nth-child(5)').css({
-						'color': 'white',
-						'background-color': '#f2bc1a',
-						'border': '2px solid #f2d264',
-						'border-radius': '2em'});
 					$("input:checked").prop('checked', false);
 				});
 			}
+			$('.question-number').find('li:nth-child(5)').addClass('active-number');
 			Question5();
 		});
 	}
 
 	function Question5 () {
 		var question5 = new QuizItem ('In what publication year did Barry return to the DCU?', '2008', ['1976', '2004', '2012']);
-
 		$('.question').text(question5.question);
-		$('.answer').text(question5.answer);
-		$('.wronganswer1').text(question5.wrongAnswers[0]);
-		$('.wronganswer2').text(question5.wrongAnswers[1]);
-		$('.wronganswer3').text(question5.wrongAnswers[2]);
+		
+		var answers = [question5.answer, question5.wrongAnswers[0], question5.wrongAnswers[1], question5.wrongAnswers[2]];
+
+		// console.log(answers);
+		console.log(question5.answer);
+		shuffle(answers);
+
+		$('#1 .answer-text').text(answers[0]);
+		$('#1 input').val(answers[0]);
+
+		$('#2 .answer-text').text(answers[1]);
+		$('#2 input').val(answers[1]);
+
+		$('#3 .answer-text').text(answers[2]);
+		$('#3 input').val(answers[2]);
+
+		$('#4 .answer-text').text(answers[3]);
+		$('#4 input').val(answers[3]);
 
 		$('#submitBtn').click(function() {
-			if($('input:checked').val() === "B") {
+			if($('input:checked').val() === question5.answer) {
 				console.log("right answer");
 
 				$('.overlay-container').show();	
+				$('#nextBtn').hide();
+				$('#startoverBtn').show();
 				$('h2').text("You're Correct!");
 				$('.pop-out img').attr('src', 'images/answer5.jpg');
 				$('.overlay-container p').text("Officially and permanently returned to life, the cities he once called home celebrate, while Allen contemplates why he has been brought back. Soon after he encounters Savitar, a speedster villain. He catches up to the villain, but when he touches him Savitar turns to dust, telling Allen that he was the beginning and also the end. At that moment a number of heroes who are connected to the Speed Force experience painful convulsions. Soon it is revealed that Allen is becoming the new Black Flash, and he flees to the Speed Force to avoid harming any innocents. There he encounters other heroes who tell him that becoming the Black Flash is not his fault, and soon Professor Zoom, appears, revealing that he was responsible not only for Allen's resurrection, but also a number of tragedies in his life, including the murder of his mother.");
 				
-				$('#nextBtn').click(function() {
+				$('#startoverBtn').click(function() {
 					$('.overlay-container').hide();
-					$('.question-number').find('li:nth-child(6)').css({
-						'color': 'white',
-						'background-color': '#f2bc1a',
-						'border': '2px solid #f2d264',
-						'border-radius': '2em'});
+					$('#nextBtn').show();
+					$('#startoverBtn').hide();
 					$("input:checked").prop('checked', false);
 				});
 			}
@@ -281,20 +286,23 @@ $(document).ready(function() {
 				console.log("wrong answer");
 
 				$('.overlay-container').show();
+				$('#nextBtn').hide();
+				$('#startoverBtn').show();
 				$('h2').text('You Were So Close...');
 				$('.pop-out img').attr('src', 'images/answer5.jpg');
 				$('.overlay-container p').text("Officially and permanently returned to life, the cities he once called home celebrate, while Allen contemplates why he has been brought back. Soon after he encounters Savitar, a speedster villain. He catches up to the villain, but when he touches him Savitar turns to dust, telling Allen that he was the beginning and also the end. At that moment a number of heroes who are connected to the Speed Force experience painful convulsions. Soon it is revealed that Allen is becoming the new Black Flash, and he flees to the Speed Force to avoid harming any innocents. There he encounters other heroes who tell him that becoming the Black Flash is not his fault, and soon Professor Zoom, appears, revealing that he was responsible not only for Allen's resurrection, but also a number of tragedies in his life, including the murder of his mother.");
 			
-				$('#nextBtn').click(function() {
+				$('#startoverBtn').click(function() {
 					$('.overlay-container').hide();
-					$('.question-number').find('li:nth-child(6)').css({
-						'color': 'white',
-						'background-color': '#f2bc1a',
-						'border': '2px solid #f2d264',
-						'border-radius': '2em'});
+					$('#nextBtn').show();
+					$('#startoverBtn').hide();
 					$("input:checked").prop('checked', false);
 				});
 			}
+			$('.question-number').find('li:nth-child(2)').removeClass('active-number');
+			$('.question-number').find('li:nth-child(3)').removeClass('active-number');
+			$('.question-number').find('li:nth-child(4)').removeClass('active-number');
+			$('.question-number').find('li:nth-child(5)').removeClass('active-number');
 			Question1();
 		});
 	}
